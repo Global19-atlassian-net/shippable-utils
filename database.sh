284a35f3e5c8cf4a0c09137a0df0e05d1a19fd61
Michael Goff <Michael.Goff@Quantum.com>
2015-02-12 16:56:57 -0800

    Improved the database script

---
+++ database.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# Author: Michael Goff <Michael.Goff@Quantum.com>
+# Licence: MIT
+# Copyright (c) 2015, Quantum Corp.
+
+usage()
+{
+cat <<EOF
+Usage:
+  $0 -h
+  $0 <command>
+
+Description:
+  Handle starting, configuring, and stopping the database for shippable
+
+Commands:
+  start - start the mysql service and config with a test user with no password
+  stop - stop the mysql service
+EOF
+}
+
+while getopts ":h" opt
+do
+  case "${opt}" in
+    h ) usage; exit 0 ;;
+    \?) echo "unrecognized option: $OPTARG" 1>&2
+        exit 1
+        ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ "$#" -ne 1 ]; then
+  echo "Invalid Params: Requires a command argument" 1>&2
+  usage
+  exit 1
+fi
+
+COMMAND=$1
+shift
+
+case "$COMMAND" in
+  start)
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+    sleep 5
+    while ! nc -vz localhost 3306 &>/dev/null; do
+      echo "Waiting for mysql to start..."
+      sleep 5
+    done
+    echo "mysql started successfully"
+    mysql -u root -e "DROP USER 'test'@'localhost';" || true
+    mysql -u root -e "FLUSH PRIVILEGES; CREATE USER 'test'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;"
+    ;;
+
+  stop)
+    mysqladmin shutdown
+    ;;
+
+  *)
+    echo "Unknown command: ${COMMAND}" 1>&2
+    usage
+    exit 1
+    ;;
+esac
--- setup-database.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mysql -u root -e "DROP USER 'test'@'localhost';" || true
-mysql -u root -e "FLUSH PRIVILEGES; CREATE USER 'test'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;"
