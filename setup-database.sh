09e6b1e6a7d511a771cf80d20ba049e96f64c427
Michael Goff <Michael.Goff@Quantum.com>
2015-02-12 16:08:03 -0800

    Make sure that drop use failing doesn't fail the script

--- setup-database.sh
+++ setup-database.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-mysql -u root -e "DROP USER 'test'@'localhost'; FLUSH PRIVILEGES; CREATE USER 'test'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;"
+mysql -u root -e "DROP USER 'test'@'localhost';" || true
+mysql -u root -e "FLUSH PRIVILEGES; CREATE USER 'test'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;"
